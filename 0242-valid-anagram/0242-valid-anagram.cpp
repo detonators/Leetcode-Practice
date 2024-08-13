7a9fe7d5e812f67cefa61ea6eefcc28e0db5416e
Shubham Singh <87699417+detonators@users.noreply.github.com>
2024-08-14 02:53:26 +0530

    Time: 6 ms (79.92%), Space: 8.8 MB (48.20%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,65 @@
+/*
+  
+            
+     |        |                   | 
+ ____|  ____ -|--  __  ___   __  -|--  __    _
+|    | | |_|  |   |  | |  | |  |  |   |  |  |
+|____| |____  |_  |__| |  | |__|_ |__ |__|  |
+_________________________________________________
+-------------------------------------------------
+
+
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<+*&-------------------&*+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+*/
+// Common Helpers
+#define pb push_back
+#define ed end()
+#define bg begin()
+#define sz size()
+#define ln length()
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define NL cout << '\n';
+#define F first
+#define S second
+#define disp(x) for(auto t : x )cout<<t<<" ";
+#define for0(n) for(int i = 0 ; i < n ; i++)
+#define for0j(n) for(int j = 0 ; j < n ; j++)
+#define oldfor(v) for(auto it = v.begin(); it != v.end(); ++it)
+#define SS << ' ' <<
+#define arrin(a,n) vi a(n);for0(n){cin >> a[i];}
+#define pf(x) cout << x << endl;
+#define gridin(num,n,m) for0(n){for0j(m) cin >> num[i][j];}
+#define dispgrid(num,n,m) for0(n){for0j(m) cout << num[i][j] << ' '; cout << endl;}
+
+#define accuracy chrono::steady_clock::now().time_since_epoch().count()
+
+
+//replace_junk
+
+// Overloaded Ostream Classes
+template<typename F, typename S>
+ostream& operator<<(ostream& os,const pair<F,S> &v){return os << "(" << v.F << ", " << v.S << ")";}
+template < typename T >
+ostream &operator<<(ostream &os,const vector<T> &v){os<< "{";oldfor(v){if(it!=v.bg)os<<", ";os<<*it;}return os << "}";}
+template < typename T >
+ostream &operator << (ostream &os, const set<T> &v){os << "[";oldfor(v){if(it!=v.bg)os<< ", ";os << *it;}return os << "]";}
+template < typename T >
+ostream &operator << (ostream &os, const multiset<T> &v){os << "[";oldfor(v){if(it!=v.bg) os << ", ";os<<*it;}return os << "]";}
+template < typename F, typename S >
+ostream &operator <<(ostream & os, const map<F,S> &v){os << "[";oldfor(v){if(it!=v.bg)os<< ", ";os<< it->F<<" = "<<it->S;}return os << "]";}
+
+//replace_junk
+
+// Solution 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(all(s)); 
+        sort(all(t));    
+        return s==t;
+        
+    }
+};
