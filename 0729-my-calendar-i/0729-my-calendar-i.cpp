9fcc8887c91120cef9cf6f3ee3c1c9bd86cb4c20
Shubham Singh <87699417+detonators@users.noreply.github.com>
2024-09-26 23:08:55 +0530

    Time: 72 ms (79.52%), Space: 42.8 MB (36.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -3,23 +3,11 @@ class MyCalendar {
     set<int2> S;
 public:
     MyCalendar() { }
-    
     bool book(int start, int end) {
         auto it=S.lower_bound({start, -1});
-        
-        // overlap with the next interval
-        if (it != S.end() && (*it).first<end) 
-            return 0;
-        
-        
-        // overlap with the previous interval
-        if (it != S.begin() && (*prev(it)).second > start) 
-            return 0;
-        
-        
-        // No overlap, insert the new booking
-        S.insert({start, end});
-        return 1;
+        if (it != S.end() && (*it).first<end) return 0;
+        if (it != S.begin() && (*prev(it)).second > start)  return 0;
+        S.insert({start, end}); return 1;
     }
 };
 
