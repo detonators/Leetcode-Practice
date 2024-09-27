d8cedc9c52d4f69b3d98615c9eeeb8597a03abc2
Shubham Singh <87699417+detonators@users.noreply.github.com>
2024-09-27 05:34:33 +0530

    Time: 59 ms (98.02%), Space: 37.6 MB (99.37%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,29 @@
+class MyCalendarTwo {
+    vector<pair<int,int>> b;
+    vector<pair<int,int>>db;
+public:
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+        for(pair<int,int> x: db){
+            if(start<x.second && end>x.first) return false;
+        }
+        for(pair<int,int> x : b){
+            if(start<x.second && end>x.first){
+                db.push_back({max(start,x.first),min(end,x.second)});
+            }
+        }
+        b.push_back({start,end});
+        return true;
+        
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
